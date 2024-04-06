9f1c842a9129dcd7413d44db82691095cc26213b
muthukumar <muthukumarece5@gmail.com>
2024-04-06 20:51:31 +0530

    Update AutoRefresh.py

--- GithubAutoRefresh/AutoRefresh.py
+++ GithubAutoRefresh/AutoRefresh.py
@@ -1,30 +1,25 @@
-'''
-Created on Nov 15, 2019
+"""
+AutoRefresh
+"""
 
-@author: Muthukumar Subramanian
-'''
-from selenium import webdriver 
+__version__ = "2019.11.15.01"
+__author__ = "Muthukumar Subramanian"
+
+from selenium import webdriver
 import time
 
 # set webdriver path here it may vary
-browser = webdriver.Chrome(executable_path ="C:\\Users\\muthu\\Documents\\Sel_driver\\Driver\\chromedriver_win32\\chromedriver.exe") 
-  
-# website_URL ="https://www.google.co.in/"
-browser.get("https://github.com/kumarmuthu/python_logic") 
-browser.maximize_window() 
+browser = webdriver.Chrome()
 
-# refresh time
-refreshrate = int(2) 
-while True: 
-    b = 0
-    for i in range(1, 6, 1):
-        print("I: ",i)
-        # This would keep running until you stop the compiler. 
-        time.sleep(refreshrate) 
-        browser.refresh() 
-        if i == 5:
-            b =1
-            print("break here")
-    if b == 1:
+website_URL = "https://github.com/kumarmuthu/PythonWithSelenium"
+browser.get(website_URL)
+browser.maximize_window()
+
+for i in range(1, 6):
+    time.sleep(2)
+    browser.refresh()
+    if i == 5:
+        print("Break here")
         break
+
 browser.close()
